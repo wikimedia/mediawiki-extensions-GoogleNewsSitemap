ad1ae9881f34b91fd2d2fff53d1c4e6153cfbc0c
Raimond Spekking <raymond@users.mediawiki.org>
2010-01-12 17:20:32 +0000

    Add Google News Sitemap to translatewiki Tweak messages a bit

--- SpecialGoogleNewsSitemap.i18n.php
+++ SpecialGoogleNewsSitemap.i18n.php
@@ -14,8 +14,8 @@ $messages= array();
 
 $messages['en'] = array(
     'gnsm'                  => 'Google News Sitemap',
-    'gnsm-desc'             => 'Outputs an Atom/RSS feed as a Google News Sitemap.',
-    'gnsm_categorymap'      => '', #default empty. list of categories to map to keywords. do not translate.
+    'gnsm-desc'             => 'Outputs an Atom/RSS feed as a Google News Sitemap',
+    'gnsm_categorymap'      => '', # Default empty. List of categories to map to keywords. Do not translate.
     'gnsm_toomanycats'      => 'Error: Too many categories!',
     'gnsm_toofewcats'       => 'Error: Too few categories!',
     'gnsm_noresults'        => 'Error: No results!',
@@ -28,9 +28,10 @@ $messages['en'] = array(
 
 $messages['fr'] = array(
     'gnsm'                  => 'Google nouvelles Sitemap',
-    'gnsm-desc'             => 'Cre un Atom ou RSS feed comme un plan Sitemap pour Google.',
+    'gnsm-desc'             => 'Cre un Atom ou RSS feed comme un plan Sitemap pour Google',
     'gnsm_toomanycats'      => 'Erreur: Trop de nombreuses catégories!',
     'gnsm_toofewcats'       => 'Erreur: Trop peu de catégories!',
     'gnsm_noresults'        => 'Erreur: Pas de résultats!',
     'gnsm_noincludecats'    => 'Erreur: Vous devez inclure au moins une catégorie, ou spécifier un nom d\'espace !'
 );
+
