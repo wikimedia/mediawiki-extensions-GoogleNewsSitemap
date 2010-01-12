2169b0368cd1fbb079e19cc143eaab6482d1d5be
Raimond Spekking <raymond@users.mediawiki.org>
2010-01-12 19:22:39 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-01-12 19:00 UTC)

--- SpecialGoogleNewsSitemap.i18n.php
+++ SpecialGoogleNewsSitemap.i18n.php
@@ -22,16 +22,51 @@ $messages['en'] = array(
     'gnsm_noincludecats'    => 'Error: You need to include at least one category, or specify a namespace!',
 );
 
-/** Français
- * @author Amgine
- **/
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'gnsm' => 'Google Nuus Sitemap',
+	'gnsm-desc' => 'Eksporteer \'n Atom/RSS-voer as \'n Google "News Sitemap"',
+	'gnsm_toomanycats' => 'Fout: Te veel kategorieë!',
+	'gnsm_toofewcats' => 'Fout: Te min kategorieë!',
+	'gnsm_noresults' => 'Fout: Geen resultate!',
+	'gnsm_noincludecats' => "Fout: U moet ten minste een kategorie insluit, of spesifiseer 'n naamspasie!",
+);
 
+/** French (Français)
+ * @author Amgine
+ */
 $messages['fr'] = array(
-    'gnsm'                  => 'Google nouvelles Sitemap',
-    'gnsm-desc'             => 'Cre un Atom ou RSS feed comme un plan Sitemap pour Google',
-    'gnsm_toomanycats'      => 'Erreur: Trop de nombreuses catégories!',
-    'gnsm_toofewcats'       => 'Erreur: Trop peu de catégories!',
-    'gnsm_noresults'        => 'Erreur: Pas de résultats!',
-    'gnsm_noincludecats'    => 'Erreur: Vous devez inclure au moins une catégorie, ou spécifier un nom d\'espace !'
+	'gnsm' => 'Google nouvelles Sitemap',
+	'gnsm-desc' => 'Cre un Atom ou RSS feed comme un plan Sitemap pour Google',
+	'gnsm_toomanycats' => 'Erreur: Trop de nombreuses catégories!',
+	'gnsm_toofewcats' => 'Erreur: Trop peu de catégories!',
+	'gnsm_noresults' => 'Erreur: Pas de résultats!',
+	'gnsm_noincludecats' => "Erreur: Vous devez inclure au moins une catégorie, ou spécifier un nom d'espace !",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'gnsm' => 'Mapa do sitio das novas do Google',
+	'gnsm-desc' => 'Dá como resultado unha fonte de novas Atom/RSS como un mapa do sitio das novas do Google',
+	'gnsm_toomanycats' => 'Erro: hai moitas categorías!',
+	'gnsm_toofewcats' => 'Erro: moi poucas categorías!',
+	'gnsm_noresults' => 'Erro: non hai resultados!',
+	'gnsm_noincludecats' => 'Erro: debe incluír, polo menos, unha categoría ou especificar un espazo de nomes!',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'gnsm' => 'Mapa dël Sit dle Neuve ëd Google',
+	'gnsm-desc' => 'Scriv un feed Atom/RSS com Mapa dël Sit ëd le Neuve ëd Google',
+	'gnsm_toomanycats' => 'Eror: Tròpe categorìe!',
+	'gnsm_toofewcats' => 'Eror: Tròp pòche categorìe!',
+	'gnsm_noresults' => 'Eror: pa gnun arzultà!',
+	'gnsm_noincludecats' => 'Eror: It deuve ansërì almench na categorìa, o spessifié në spassi nominal!',
 );
 
