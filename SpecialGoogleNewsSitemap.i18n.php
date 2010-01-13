d0d774be0b0532d9f32aa4bd18406762e752266d
Raimond Spekking <raymond@users.mediawiki.org>
2010-01-13 19:30:11 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-01-13 19:00 UTC)

--- SpecialGoogleNewsSitemap.i18n.php
+++ SpecialGoogleNewsSitemap.i18n.php
@@ -34,6 +34,30 @@ $messages['af'] = array(
 	'gnsm_noincludecats' => "Fout: U moet ten minste een kategorie insluit, of spesifiseer 'n naamspasie!",
 );
 
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'gnsm' => 'Мапа сайту Google News',
+	'gnsm-desc' => 'Выводзіць стужкі Atom/RSS у выглядзе мапы сайту Google News',
+	'gnsm_toomanycats' => 'Памылка: зашмат катэгорыяў!',
+	'gnsm_toofewcats' => 'Памылка: занадта мала катэгорыяў!',
+	'gnsm_noresults' => 'Памылка: няма вынікаў!',
+	'gnsm_noincludecats' => 'Памылка: Вам неабходна дадаць хаця б адну катэгорыю, альбо пазначыць прастору назваў!',
+);
+
+/** Breton (Brezhoneg)
+ * @author Y-M D
+ */
+$messages['br'] = array(
+	'gnsm' => 'Google Keleier Sitemap',
+	'gnsm_toomanycats' => 'Fazi : Re a rummadoù !',
+	'gnsm_toofewcats' => 'Fazi : Re nebeut a rummadoù !',
+	'gnsm_noresults' => "Fazi : Disoc'h ebet !",
+	'gnsm_noincludecats' => "Fazi : Rankout a reoc'h lakaat ur rummad d'an nebeutañ, pe menegiñ un esaouenn anv !",
+);
+
 /** French (Français)
  * @author Amgine
  */
@@ -58,15 +82,81 @@ $messages['gl'] = array(
 	'gnsm_noincludecats' => 'Erro: debe incluír, polo menos, unha categoría ou especificar un espazo de nomes!',
 );
 
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'gnsm' => 'Google Nejigkeite Sytenibersicht',
+	'gnsm-desc' => 'Liferet e Atom/RSS-feed as Google Nejigkeite Sytenibersicht',
+	'gnsm_toomanycats' => 'Fähler: z vil Kategorie!',
+	'gnsm_toofewcats' => 'Fähler: z wenig Kategorie!',
+	'gnsm_noresults' => 'Fähler: kei Ergebnis!',
+	'gnsm_noincludecats' => 'Fähler: muesch zmindescht ei Kategorii aagee oder e Namensruum feschtlege!',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'gnsm_toomanycats' => 'Feeler: Zevill Kategorien!',
+	'gnsm_toofewcats' => 'Feeler: Ze wéineg Kategorien!',
+	'gnsm_noresults' => 'Feeler: Keng Resultater!',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'gnsm' => 'План на страницата Google Вести',
+	'gnsm-desc' => 'Дава Atom/RSS канал како план на страницата Google Вести',
+	'gnsm_toomanycats' => 'Грешка: Премногу категории!',
+	'gnsm_toofewcats' => 'Грешка: Премалку категории!',
+	'gnsm_noresults' => 'Грешка: Нема резултати!',
+	'gnsm_noincludecats' => 'Грешка: Треба да вклучите барем една категорија, или да назначите именски простор!',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'gnsm' => 'Google Nieuws Sitemap',
+	'gnsm-desc' => 'Levert een Atom/RSS-feed als Google Nieuws Sitemap',
+	'gnsm_toomanycats' => 'Fout: te veel categorieën!',
+	'gnsm_toofewcats' => 'Fout: te weinig categorieën!',
+	'gnsm_noresults' => 'Fout: geen resultaten!',
+	'gnsm_noincludecats' => 'Fout: u moet tenminste een categorie of naamruimte opgeven!',
+);
+
 /** Piedmontese (Piemontèis)
+ * @author Borichèt
  * @author Dragonòt
  */
 $messages['pms'] = array(
-	'gnsm' => 'Mapa dël Sit dle Neuve ëd Google',
-	'gnsm-desc' => 'Scriv un feed Atom/RSS com Mapa dël Sit ëd le Neuve ëd Google',
+	'gnsm' => 'Pian dël sit dle Neuve ëd Google',
+	'gnsm-desc' => 'A scriv un fluss Atom/RSS com pian dël Sit ëd le Neuve ëd Google',
 	'gnsm_toomanycats' => 'Eror: Tròpe categorìe!',
 	'gnsm_toofewcats' => 'Eror: Tròp pòche categorìe!',
 	'gnsm_noresults' => 'Eror: pa gnun arzultà!',
-	'gnsm_noincludecats' => 'Eror: It deuve ansërì almench na categorìa, o spessifié në spassi nominal!',
+	'gnsm_noincludecats' => 'Eror: A deuv anserì almanch na categorìa, o spessifiché në spassi nominal!',
+);
+
+/** Swedish (Svenska)
+ * @author Per
+ */
+$messages['sv'] = array(
+	'gnsm_toomanycats' => 'Fel: För många kategorier!',
+	'gnsm_toofewcats' => 'Fel: För få kategorier!',
+	'gnsm_noresults' => 'Fel: Inget resultat!',
+	'gnsm_noincludecats' => 'Fel: Du måste inkludera minst en kategori eller specificera en namnrymd!',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'gnsm' => 'గూగుల్ వార్తల సైటుపటం',
+	'gnsm_toomanycats' => 'పొరపాటు: చాలా ఎక్కువ వర్గాలు!',
+	'gnsm_toofewcats' => 'పొరపాటు: చాలా తక్కువ వర్గాలు!',
+	'gnsm_noresults' => 'పొరపాటు: ఫలితాలు లేవు!',
 );
 
