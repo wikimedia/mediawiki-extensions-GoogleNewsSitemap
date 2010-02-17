08de0ab44d7d220e04bc18d44a5b15bc0ca938e0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-02-17 20:52:27 +0000

    Localisation updates for special page aliases for extensions from translatewiki.net

--- SpecialGoogleNewsSitemap.alias.php
+++ SpecialGoogleNewsSitemap.alias.php
@@ -14,3 +14,18 @@ $aliases['en'] = array(
     'GoogleNewsSitemap' => array( 'GoogleNewsSitemap' ),
 );
 
+$aliases['ar'] = array(
+	'GoogleNewsSitemap'   => array( 'خريطة_موقع_أخبار_جوجل' ),
+);
+
+$aliases['arz'] = array(
+	'GoogleNewsSitemap'   => array( 'خريطة_موقع_أخبار_جوجل' ),
+);
+
+$aliases['ia'] = array(
+	'GoogleNewsSitemap'   => array( 'Mappa de sito Google News' ),
+);
+
+$aliases['ml'] = array(
+	'GoogleNewsSitemap'   => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
+);
