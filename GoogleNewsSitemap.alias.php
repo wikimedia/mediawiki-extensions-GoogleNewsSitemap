245c4c1bf80b50f06055d90d8ada53c7b649f3fa
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-05-17 18:29:36 +0000

    Localisation updates for extension special page aliases and magic words from translatewiki.net * some non-essential language header eye candy was removed because these exports were made with the (slow!) magic-export.php, and not with the broken alias-export.php

--- GoogleNewsSitemap.alias.php
+++ GoogleNewsSitemap.alias.php
@@ -14,23 +14,22 @@ $aliases['en'] = array(
     'GoogleNewsSitemap' => array( 'GoogleNewsSitemap' ),
 );
 
-/** Arabic (العربية) */
 $aliases['ar'] = array(
-	'GoogleNewsSitemap' => array( 'خريطة_موقع_أخبار_جوجل' ),
+	'GoogleNewsSitemap'   => array( 'خريطة_موقع_أخبار_جوجل' ),
 );
 
-/** Egyptian Spoken Arabic (مصرى) */
 $aliases['arz'] = array(
-	'GoogleNewsSitemap' => array( 'خريطة_موقع_أخبار_جوجل' ),
+	'GoogleNewsSitemap'   => array( 'خريطة_سايت_اخبار_جوجل' ),
 );
 
-/** Interlingua (Interlingua) */
 $aliases['ia'] = array(
-	'GoogleNewsSitemap' => array( 'Mappa de sito Google News' ),
+	'GoogleNewsSitemap'   => array( 'Mappa de sito Google News' ),
 );
 
-/** Malayalam (മലയാളം) */
+$aliases['ja'] = array(
+	'GoogleNewsSitemap'   => array( 'Googleニュースサイトマップ' ),
+);
+
 $aliases['ml'] = array(
-	'GoogleNewsSitemap' => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
+	'GoogleNewsSitemap'   => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
 );
-
