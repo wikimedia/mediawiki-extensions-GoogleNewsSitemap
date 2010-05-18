b8d573687131bc31d9fa3d804d1021c03ce60d5e
Raimond Spekking <raymond@users.mediawiki.org>
2010-05-18 20:00:55 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-05-18 19:45:00 UTC)

--- GoogleNewsSitemap.alias.php
+++ GoogleNewsSitemap.alias.php
@@ -14,22 +14,28 @@ $aliases['en'] = array(
     'GoogleNewsSitemap' => array( 'GoogleNewsSitemap' ),
 );
 
+/** Arabic (العربية) */
 $aliases['ar'] = array(
-	'GoogleNewsSitemap'   => array( 'خريطة_موقع_أخبار_جوجل' ),
+	'GoogleNewsSitemap' => array( 'خريطة_موقع_أخبار_جوجل' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $aliases['arz'] = array(
-	'GoogleNewsSitemap'   => array( 'خريطة_سايت_اخبار_جوجل' ),
+	'GoogleNewsSitemap' => array( 'خريطة_سايت_اخبار_جوجل' ),
 );
 
+/** Interlingua (Interlingua) */
 $aliases['ia'] = array(
-	'GoogleNewsSitemap'   => array( 'Mappa de sito Google News' ),
+	'GoogleNewsSitemap' => array( 'Mappa de sito Google News' ),
 );
 
+/** Japanese (日本語) */
 $aliases['ja'] = array(
-	'GoogleNewsSitemap'   => array( 'Googleニュースサイトマップ' ),
+	'GoogleNewsSitemap' => array( 'Googleニュースサイトマップ' ),
 );
 
+/** Malayalam (മലയാളം) */
 $aliases['ml'] = array(
-	'GoogleNewsSitemap'   => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
+	'GoogleNewsSitemap' => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
 );
+
