132f616daa2c6ec6c274fabf6379a78ac726985a
Chad Horohoe <demon@users.mediawiki.org>
2010-05-26 22:25:32 +0000

    Large commit. Removed 800+ references to no-op wfLoadExtensionMessages()

--- GoogleNewsSitemap_body.php
+++ GoogleNewsSitemap_body.php
@@ -77,7 +77,6 @@ class GoogleNewsSitemap extends IncludableSpecialPage {
 		global $wgContLang;
 		global $wgRequest, $wgOut;
 		global $wgSitename, $wgServer, $wgScriptPath;
-		wfLoadExtensionMessages( 'GoogleNewsSitemap' );
 		global $wgFeedClasses, $wgLocaltimezone;
 
 		// Not sure how clean $wgLocaltimezone is
