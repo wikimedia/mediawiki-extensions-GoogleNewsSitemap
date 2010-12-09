2adbcbd2f951bda6d1f65460dc536628bed7fa26
Sam Reed <reedy@users.mediawiki.org>
2010-12-09 02:25:18 +0000

    Followup r78108, revert i18n and Alias changes, no point reverting those

--- GoogleNewsSitemap.alias.php
+++ GoogleNewsSitemap.alias.php
@@ -1,42 +1,76 @@
 <?php
 /**
- * Aliases for Special:Editcount
+ * Aliases for Special:GoogleNewsSitemap
  *
- * @addtogroup Extensions
+ * @file
+ * @ingroup Extensions
  */
 
-/**
- * Deutsch
- * @author Amgine
- **/
-$aliases['de'] = array(
-    'GNSM' => array( 'SpezialGNSM' )
-);
+$specialPageAliases = array();
 
 /** English
  * @author Amgine
- **/
-$aliases['en'] = array(
-    'GNSM' => array( 'Google News SiteMap', 'SpecialGNSM' ),
+ */
+$specialPageAliases['en'] = array(
+	'GoogleNewsSitemap' => array( 'GoogleNewsSitemap' ),
 );
 
-/** Français
- * @author Amgine
- **/
-$aliases['fr'] = array(
-    'GNSM' => array( 'GNSMSpécial' ),
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'GoogleNewsSitemap' => array( 'خريطة_موقع_أخبار_جوجل' ),
 );
 
-/** Nederlands
- * @author Amgine
- **/
-$aliases['nl'] = array(
-    'GNSM' => array( 'GNSMSpeciaal' )
+/** Egyptian Spoken Arabic (مصرى) */
+$specialPageAliases['arz'] = array(
+	'GoogleNewsSitemap' => array( 'خريطة_سايت_اخبار_جوجل' ),
 );
 
-/** Norsk (bokmål)
- * @author Amgine
- **/
-$aliases['no'] = array(
-	'GNSM' => array( 'SpesialGNSM' ),
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = array(
+	'GoogleNewsSitemap' => array( 'نقشه_وب‌گاه_گوگل_نیوز' ),
 );
+
+/** Hungarian (Magyar) */
+$specialPageAliases['hu'] = array(
+	'GoogleNewsSitemap' => array( 'Google_Hírek_oldaltérkép' ),
+);
+
+/** Interlingua (Interlingua) */
+$specialPageAliases['ia'] = array(
+	'GoogleNewsSitemap' => array( 'Mappa_de_sito_Google_News' ),
+);
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = array(
+	'GoogleNewsSitemap' => array( 'Peta_situs_Google_News', 'PetaSitusGoogleNews' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'GoogleNewsSitemap' => array( 'Googleニュースサイトマップ' ),
+);
+
+/** Ladino (Ladino) */
+$specialPageAliases['lad'] = array(
+	'GoogleNewsSitemap' => array( 'Mapa_de_sitio_de_GoogleJhabberes' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'GoogleNewsSitemap' => array( 'ഗൂഗിൾ‌‌വാർത്തകൾസൈറ്റ്മാപ്പ്' ),
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
+$specialPageAliases['no'] = array(
+	'GoogleNewsSitemap' => array( 'Google_nyheter-sidekart' ),
+);
+
+/** Polish (Polski) */
+$specialPageAliases['pl'] = array(
+	'GoogleNewsSitemap' => array( 'Mapa_strony_dla_Google_News' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
